e900705aadb075c9df4edf8ccd0ed320a319925c
Santiago Enciso <whereispolaris@gmail.com>
2019-05-11 14:12:07 -0500

    Updated react elements to use JSX, updated README.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # TO DO APP
 
     Building a simple To Do app with React.js
+
+### Running the app on a local environment with Python's SimpleHTTPServer module:
+
+```
+$ python -m SimpleHTTPServer
+```
+http://localhost:8000
+
+Documentation:
+https://developer.mozilla.org/en-US/docs/Learn/Common_questions/set_up_a_local_testing_server#Running_a_simple_local_HTTP_server
--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const title = React.createElement(
-    'h1',
-    { id: 'main-title', title: 'This is the title.'},
-    'My first React Element!'
+// const title = React.createElement(
+//     'h1',
+//     { id: 'main-title', title: 'This is the title.'},
+//     'My first React Element!'
+// );
+const title = <h1>My first React Element!</h1>;
+
+
+// const desc = React.createElement(
+//     'p',
+//     null,
+//     'I just leared how to create a react node and reder it into the DOM!'
+// );
+const desc = <p>I just leared how to create a react node and reder it into the DOM.</p>;
+
+const header = React.createElement(
+    'header',
+    null,
+    title,
+    desc,
 );
 
 ReactDOM.render(
-    title,
+    header,
     document.getElementById('root')
 );
