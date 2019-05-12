cace1c958884b811022fcb2bc56ef94bbc26a614
Santiago Enciso <whereispolaris@gmail.com>
2019-05-12 14:52:41 -0500

    Remove comments, created Header compontent and moved title and description to it.

--- app.js
+++ app.js
@@ -1,35 +1,13 @@
-// const title = React.createElement(
-//     'h1',
-//     { id: 'main-title', title: 'This is the title.'},
-//     'My first React Element!'
-// );
-const title = 'My first React Element!';
-
-
-// const desc = React.createElement(
-//     'p',
-//     null,
-//     'I just leared how to create a react node and reder it into the DOM!'
-// );
-const desc = 'I just leared how to create a react node and reder it into the DOM.';
-
-const myTitleID = 'main-title';
-const name = 'Santi';
-// const header = React.createElement(
-//     'header',
-//     null,
-//     title,
-//     desc,
-// );
-const header = (
-    <header>
-        <h1 id={myTitleID}>{name}'s first Reactl Element!</h1>
-        <p className="main-desc">{desc}</p>
-    </header>
-);
-
+function Header() {
+    return (
+        <header>
+            <h1>To Do</h1>
+            <span className="stats">Tasks: 1</span>
+        </header>
+    );
+}
 
 ReactDOM.render(
-    header,
+    <Header />,
     document.getElementById('root')
 );
